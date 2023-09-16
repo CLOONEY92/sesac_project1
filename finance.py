ce02e20cd8a20e89fd05884df686785b685ffedd
CLOONEY92 <kurashikijun@gmail.com>
2023-09-16 21:31:39 +0900

    csv파일수정코드

--- finance.py
+++ finance.py
@@ -5,6 +5,7 @@ import pandas as pd
 import csv
 file = open("stock.csv", mode='w', encoding="utf-8", newline="")
 writer = csv.writer(file)
+writer.writerow(['날짜', '종가'])
 
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
@@ -12,7 +13,7 @@ headers = {
 
 datelist = []
 pricelist = []
-pops = []
+
 for page in range(2, 5):
     page_url = f'https://finance.naver.com/item/sise_day.naver?code=005930&page={page}'
     page_res = requests.get(page_url, headers=headers)
@@ -27,11 +28,8 @@ for page in range(2, 5):
             datelist.append(date)
             pricelist.append(stock_price)
 
-            pops.append(date)
-            pops.append(stock_price)
+            writer.writerow([date, stock_price])
 # print(pops)
-writer.writerow(['날짜', '종가'])
-writer.writerow(pops)
 file.close()
 
 dataframe = {'날짜' : datelist, '종가' : pricelist}
